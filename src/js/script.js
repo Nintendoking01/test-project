ce78feb956d17d8384df9a8d6a2fb9f571d9458d
Timur <tsimurmokharau@gmail.com>
2024-10-15 22:46:15 +0200

    the last commit 15.10

--- src/js/script.js
+++ src/js/script.js
@@ -10,12 +10,33 @@ let personalMovieDb = {
     privat: false
 };
 
-let questionNumberOne = prompt("What was the last movie you watched?", ""),
-    questionNumberTwo = +prompt("How much would you rate it?", ""),
-    questionNumberThree = prompt("What was the last movie you watched?", ""),
-    questionNumberFour = +prompt("How much would you rate it?", "");
 
-personalMovieDb.movies[questionNumberOne] = questionNumberTwo
-personalMovieDb.movies[questionNumberThree] = questionNumberFour
+// questionNumberThree = prompt("What was the last movie you watched?", ""),
+//     questionNumberFour = +prompt("How much would you rate it?", "");
 
-console.log(personalMovieDb)
+
+
+for (let i = 0; i < 2; i++) {
+    let questionNumberOne = prompt("What was the last movie you watched?", ""),
+        questionNumberTwo = +prompt("How much would you rate it?", "");
+
+    if (questionNumberOne != null && questionNumberTwo != null && questionNumberOne != '' && questionNumberTwo != '' && questionNumberOne.length < 50) {
+        personalMovieDb.movies[questionNumberOne] = questionNumberTwo;
+        console.log("Done")
+    } else {
+        console.log('error');
+        i--;
+    }
+};
+if (personalMovieDb.count < 10) {
+    console.log("Watched quite a few films");
+} else if (personalMovieDb.count >= 10 && personalMovieDb < 30) {
+    console.log("Are you a classic viewer");
+} else if (personalMovieDb.count >= 30) {
+    console.log("Are you a film buff?");
+} else {
+    console.log("An error has occurred");
+}
+
+console.log(personalMovieDb);
+
