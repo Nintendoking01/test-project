4d7771955f755ed363cd3fb3926e0c4132da1a04
Timur <tsimurmokharau@gmail.com>
2024-10-16 14:56:42 +0200

    the mod function 16.10

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,14 @@
 "use strict";
-let numberOfFilms = +prompt("How many films are you watched?", "");
-console.log(numberOfFilms)
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("How many films are you watched?", "");
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("How many films are you watched?", "");
+    }
+}
+
+start();
 
 let personalMovieDb = {
     count: numberOfFilms,
@@ -10,33 +18,48 @@ let personalMovieDb = {
     privat: false
 };
 
-
-// questionNumberThree = prompt("What was the last movie you watched?", ""),
-//     questionNumberFour = +prompt("How much would you rate it?", "");
-
-
-
-for (let i = 0; i < 2; i++) {
-    let questionNumberOne = prompt("What was the last movie you watched?", ""),
-        questionNumberTwo = +prompt("How much would you rate it?", "");
-
-    if (questionNumberOne != null && questionNumberTwo != null && questionNumberOne != '' && questionNumberTwo != '' && questionNumberOne.length < 50) {
-        personalMovieDb.movies[questionNumberOne] = questionNumberTwo;
-        console.log("Done")
-    } else {
-        console.log('error');
-        i--;
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDb);
     }
-};
-if (personalMovieDb.count < 10) {
-    console.log("Watched quite a few films");
-} else if (personalMovieDb.count >= 10 && personalMovieDb < 30) {
-    console.log("Are you a classic viewer");
-} else if (personalMovieDb.count >= 30) {
-    console.log("Are you a film buff?");
-} else {
-    console.log("An error has occurred");
+}
+showMyDB();
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        let questionNumberOne = prompt("What was the last movie you watched?", ""),
+            questionNumberTwo = +prompt("How much would you rate it?", "");
+
+        if (questionNumberOne != null && questionNumberTwo != null && questionNumberOne != '' && questionNumberTwo != '' && questionNumberOne.length < 50) {
+            personalMovieDb.movies[questionNumberOne] = questionNumberTwo;
+            console.log("Done")
+        } else {
+            console.log('error');
+            i--;
+        }
+    };
 }
 
+rememberMyFilms();
+function detectPersonalLevel() {
+    if (personalMovieDb.count < 10) {
+        console.log("Watched quite a few films");
+    } else if (personalMovieDb.count >= 10 && personalMovieDb < 30) {
+        console.log("Are you a classic viewer");
+    } else if (personalMovieDb.count >= 30) {
+        console.log("Are you a film buff");
+    } else {
+        console.log("An error has occurred");
+    }
+}
+
+detectPersonalLevel();
 console.log(personalMovieDb);
 
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        const genre = prompt(`Your favorite genre under the number ${i}`);
+        personalMovieDb.genres[i - 1] = genre;
+    }
+}
+
+writeYourGenres();
